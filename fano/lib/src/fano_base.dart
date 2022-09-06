8aa7155e3671456466140b945f41b12c04641f7f
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-09-05 23:54:43 -0600

    Methods added and notice errors
    There's a problem with the Dict pointer that almost every function is receiving, but I think I can solve it with something like a singleton could be? Just an idea, I'll see what could I do.

--- fano/lib/src/fano_base.dart
+++ fano/lib/src/fano_base.dart
@@ -1,5 +1,7 @@
 // TODO: Put public facing types in this file.
 
+import 'dart:html';
+import 'dart:io';
 import 'dart:js_util';
 
 /// Checks if you are awesome. Spoiler: you are.
@@ -8,25 +10,25 @@ import 'dart:js_util';
 //}
 
 class Dict {
-  List<int>? b;
-  int? sizeLValue;
-  int? sizeH;
-  int? n;
-  int? lMask;
-  int? maxValue;
-  int? pValue;
-  int? cap;
+  List<int> b = []; // Elias-Fano representation of the bit sequence
+  int sizeLValue = 0; // bit length of a lValue
+  int sizeH = 0; // bit length of the higher bits array H
+  int n = 0; // number of entries in the dictionary
+  int lMask = 0; // mask to isolate lValue
+  int maxValue = 0; // universe of the Elias-Fano algorithm
+  int pValue = 0; // previously added value: helper value to check monotonicity
+  int cap = 0; // number of entries that can be stored in the dictionary
 }
 
-//
-
+// New constructs a dictionary with a capacity of n entries. The dictionary
+// can accept positive numbers, smaller than or equal to maxValue. The values
+// needed to be in monotonic, non-decreasing way.
 Dict New(int cap, int maxValue) {
   print("doing it");
 
   try {
     if (cap == 0) {
-      throw new Exception(
-          "ef: dictionary does not support an empty values list");
+      throw Exception("ef: dictionary does not support an empty values list");
     }
   } catch (e) {
     print(e);
@@ -49,6 +51,7 @@ Dict New(int cap, int maxValue) {
   return Dict();
 }
 
+// max0 returns the maximum of x and 0.
 int max0(int x) {
   if (x <= 0) {
     return 0;
@@ -56,16 +59,19 @@ int max0(int x) {
   return x;
 }
 
-int? Len(Dict d) {
+// Len returns the current number of entries in the dictionary.
+// When the dictionary is built with From, Len and Cap always
+// return the same result.
+int Len(Dict d) {
   //Need to check how Len works.
   return d.n;
 }
 
+// From constructs a dictionary from a list of values.
 Dict? From(var values) {
   try {
     if (values.empty) {
-      throw new Exception(
-          "ef: dictionary does not support an empty values list");
+      throw Exception("ef: dictionary does not support an empty values list");
     }
   } catch (e) {
     print(e);
@@ -77,15 +83,18 @@ Dict? From(var values) {
   return null; //missing build
 }
 
+// build encodes a monotone increasing array of positive integers
+// into an Elias-Fano bit sequence. build will return an error
+// when the values are not increasing monotonically.
 Dict? build(var values, Dict d) {
   int vmin = 0;
 
-  int? offset = d.sizeH;
+  int offset = d.sizeH;
 
   for (var i = 0; i < values.length; i++) {
     try {
       if (values[i] < vmin) {
-        throw new Exception(
+        throw Exception(
             "ef: dictionary requires an array that increases monotonically");
       }
     } catch (e) {
@@ -94,20 +103,54 @@ Dict? build(var values, Dict d) {
     }
     vmin = values[i];
 
-    int? hValue = (values[i] >> d.sizeLValue) + i.toInt();
-
-    d.b[offset >> 6] |
-        1 <<
-            (hValue &
-                63); //Problem here, need to know how to fix that keeping it nullable.
+    //higher bits processing
+    int hValue = (values[i] >> d.sizeLValue) + i.toInt();
+    d.b[offset >> 6] |= 1 << (hValue & 63);
     if (d.sizeLValue == 0) {
       continue;
     }
 
-    int? lValue = values[i] & d.lMask;
-    d.b[offset >> 6] |
-        lValue <<
-            (offset &
-                63); //Problem here, need to know how to fix that keeping it nullable.
+    //lower bits processing
+    int lValue = values[i] & d.lMask;
+    d.b[offset >> 6] |= lValue << (offset & 63);
+    int msb = lValue >> (64 - offset & 63);
+    if (msb != 0) {
+      d.b[offset >> 6 + 1] = msb;
+    }
+    offset += d.sizeLValue;
   }
+}
+
+// Append appends a value to the dictionary and returns the key. If something
+// goes wrong, Append returns -1. In that case, no value is added.
+int Append(int value, Dict d) {// TODO: Return vervangen door error. Definieer een err var voor elk soort error.
+
+  // check values
+  if(d.n != 0 && (d.cap <= d.n || value < d.pValue || d.maxValue < value)){
+    return -1;
+  }
+  d.pValue = value;
+  d.n++;
+
+  // higher bits processing
+  int hValue = value>>d.sizeLValue + (d.n-1).toInt();
+  d.b[hValue>>6] |= 1 << (hValue & 63)
+  if(d.sizeLValue == 0){
+    return d.n - 1;
+  }
+
+  // lower bits processing
+  int lValue = value & d.lMask;
+  int offset = d.sizeH + (d.n-1)*d.sizeLValue;
+  d.b[offset>>6] |= lValue << (offset & 63);
+  int msb = lValue >> (64 - offset & 63);
+  if(msb != 0){
+    d.b[offset>>6+1] = msb;
+  }
+  return d.n - 1;
+}
+
+// Cap returns the maximum number of entries in the dictionary.
+int Cap(Dict d){
+  return d.cap;
 }
