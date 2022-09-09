c5b4dcfa06f55dc383477b89d236b1d7ce2e87b0
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-09-09 09:33:33 -0600

    Added a package and some code

--- fano/lib/src/select.dart
+++ fano/lib/src/select.dart
@@ -1,21 +1,81 @@
-import 'dart:typed_data';
+import "Dict.dart";
+import 'package:bitcount/bitcount.dart';
 
-import 'package:fano/fano.dart';
-import 'package:xpx_chain_sdk/xpx_chain_sdk.dart';
-
+// hValue returns the higher bit value (bucket value) of the i-th number.
 int hValue(int i, Dict d) {
   int nOnes = i;
 
-  for (j = 0; j < d.b.length; j++) {
-    int ones = d.b.bitCount();
-    if ((i - ones) < 0) {}
+  for (var j = 0; j < d.b.length; j++) {
+    int ones = d.b[j].bitCount();
+    if ((i - ones) < 0) {
+      return (select64(d.b[j], i) + j << 6 - nOnes);
+    }
+
+    i -= ones;
   }
+  return 0;
 }
 
-int select64(int x0, k int) {
+int select1(int i, Dict d) {
+  for (var j = 0; j < d.b.length; j++) {
+    int ones = d.b[j].bitCount();
+    if ((i - ones) < 0) {
+      return (select64(d.b[j], i) + j << 6);
+    }
+    i -= ones;
+  }
+  return 0;
+}
+
+select0(int i, Dict d) {
+  for (var j = 0; j < d.b.length; j++) {
+    int xorOp = -1 ^ d.b[j];
+    int zeros = xorOp.bitCount();
+    if ((i - zeros) < 0) {
+      return (select64(xorOp, i) + j << 6);
+    }
+    i -= zeros;
+  }
+  return 0;
+}
+
+int select64(int x0, int k) {
   int x1 = (x0 & 0x5555555555555555) + (x0 >> 1 & 0x5555555555555555);
   int x2 = (x1 & 0x3333333333333333) + (x1 >> 2 & 0x3333333333333333);
   int x3 = (x2 & 0x0F0F0F0F0F0F0F0F) + (x2 >> 4 & 0x0F0F0F0F0F0F0F0F);
   int x4 = (x3 & 0x00FF00FF00FF00FF) + (x3 >> 8 & 0x00FF00FF00FF00FF);
   int x5 = (x4 & 0x0000FFFF0000FFFF) + (x4 >> 16 & 0x0000FFFF0000FFFF);
+
+  int ret = 0;
+  int t = x5 >> ret & 0xFFFFFFFF;
+  if (t <= k) {
+    k -= t;
+    ret += 32;
+  }
+  t = x4 >> ret & 0xFFFF;
+  if (t <= k) {
+    k -= t;
+    ret += 16;
+  }
+  t = x3 >> ret & 0xFF;
+  if (t <= k) {
+    k -= t;
+    ret += 8;
+  }
+  t = x2 >> ret & 0xF;
+  if (t <= k) {
+    k -= t;
+    ret += 4;
+  }
+  t = x1 >> ret & 0x3;
+  if (t <= k) {
+    k -= t;
+    ret += 2;
+  }
+  t = x0 >> ret & 0x1;
+  if (t <= k) {
+    k -= t;
+    ret++;
+  }
+  return ret;
 }
