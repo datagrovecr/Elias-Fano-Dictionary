d589c3602b4b4b034e7cd6a611019257b3872d97
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-09-09 14:28:05 -0600

    Missing just 3 methods (Values(), hValue2() and Value2())

--- fano/lib/src/dict.dart
+++ fano/lib/src/dict.dart
@@ -5,7 +5,9 @@ import 'dart:html';
 import 'dart:io';
 import 'dart:js_util';
 import 'dart:typed_data';
-//import 'package:xpx_chain_sdk/xpx_sdk.dart';
+import 'package:xpx_chain_sdk/xpx_chain_sdk.dart';
+import 'package:fano/src/select.dart';
+import 'package:tuple/tuple.dart';
 
 /// Checks if you are awesome. Spoiler: you are.
 //class Awesome {
@@ -26,7 +28,7 @@ class Dict {
 // New constructs a dictionary with a capacity of n entries. The dictionary
 // can accept positive numbers, smaller than or equal to maxValue. The values
 // needed to be in monotonic, non-decreasing way.
-Dict New(int cap, int maxValue) {
+Tuple2<Dict, Object?> New(int cap, int maxValue) {
   print("doing it");
 
   try {
@@ -35,7 +37,7 @@ Dict New(int cap, int maxValue) {
     }
   } catch (e) {
     print(e);
-    return Dict();
+    return Tuple2(Dict(), e);
   }
 
   int sizeLVal = max0(((maxValue / cap) - 1).toInt().bitLength); //Returns the minimum number of bits required to store this integer.
@@ -49,25 +51,25 @@ Dict New(int cap, int maxValue) {
   dict.maxValue = maxValue;
   dict.cap = cap;
 
-  return Dict();
+  return Tuple2(Dict(), null);
 }
 
 // From constructs a dictionary from a list of values.
-Dict? From(var values) {
+Tuple2<Dict?,Object?> From(var values) {
   try {
     if (values.empty) {
       throw Exception("ef: dictionary does not support an empty values list");
     }
   } catch (e) {
     print(e);
-    return null;
+    return Tuple2(null, e);
   }
   try {
-    Dict d = New(values.length, values[values.length - 1]);
-    return build(values, d);
+    Dict d = New(values.length, values[values.length - 1]).item1;
+    return Tuple2(build(values, d), null);
   } catch (e) {
     print(e);
-    return null;
+    return Tuple2(null, e);
   }
 }
 
@@ -84,7 +86,7 @@ int Append(int value, Dict d) {
   d.n++;
 
   // higher bits processing
-  Uint64 hValue = value >> d.sizeLValue + (d.n - 1).toInt();
+  int hValue = value >> d.sizeLValue + (d.n - 1).toInt();
   d.b[hValue >> 6] |= 1 << (hValue & 63);
   if (d.sizeLValue == 0) {
     return d.n - 1;
@@ -116,7 +118,7 @@ int Len(Dict d) {
 // build encodes a monotone increasing array of positive integers
 // into an Elias-Fano bit sequence. build will return an error
 // when the values are not increasing monotonically.
-Dict? build(var values, Dict d) {
+Tuple2<Dict?,Object?> build(var values, Dict d) {
   int vmin = 0;
 
   int offset = d.sizeH;
@@ -128,7 +130,7 @@ Dict? build(var values, Dict d) {
       }
     } catch (e) {
       print(e);
-      return null;
+      return Tuple2(null, e);
     }
     vmin = values[i];
 
@@ -151,7 +153,7 @@ Dict? build(var values, Dict d) {
 
   d.n = values.length;
 
-  return d;
+  return Tuple2(d, null);
 }
 
 // max0 returns the maximum of x and 0.
@@ -162,10 +164,71 @@ int max0(int x) {
   return x;
 }
 
-//int Value(int k, Dict d) {
-//  if (k < 0 || d.n <= k) {
-//    return 0;
-//  }
-//  return d.hValue(k) << d.sizeLValue | d.lValue(k);
-//}
+Tuple2<int, bool> Value(int k, Dict d) {
+  if (k < 0 || d.n <= k) {
+    return Tuple2(0, false);
+  }
+  return Tuple2(hValue(k, d) << d.sizeLValue | lValue(k, d), true);
+}
 
+int lValue(int k, Dict d) {
+  int offset = d.sizeH + k * d.sizeLValue;
+  int off63 = offset & 63;
+  int val = d.b[offset >> 6] >> off63 & d.lMask;
+
+  if (off63 + d.sizeLValue > 64) {
+    val |= d.b[offset >> 6 + 1] << (64 - off63) & d.lMask;
+  }
+  return val;
+}
+
+// NextGEQ returns the first value in the dictionary equal to or larger than
+// the given value. NextGEQ fails when there exists no value larger than or
+// equal to value. If so, it returns -1 as index.
+Tuple2<int, int> NextGEQ(int value, Dict d) {
+  if (d.maxValue < value) {
+    return Tuple2(-1, d.maxValue);
+  }
+
+  int hValue = (value >> d.sizeLValue).toInt();
+  // pos denotes the end of bucket hValue-1
+  int pos = select0(hValue, d);
+  // k is the number of integers whose high bits are less than hValue.
+  // So, k is the key of the first value we need to compare to.
+  int k = pos - hValue;
+  int pos63 = pos & 63;
+  int pos64 = pos & ~63;
+
+  int buf = d.b[pos64 >> 6] & ~(1 << pos63 - 1);
+  // This does not mean that the p+1-th value is larger than or equal to value!
+  // The p+1-th value has either the same hValue or a larger one. In case of a
+  // larger hValue, this value is the result we are seeking for. In case the
+  // hValue is identical, we need to scan the bucket to be sure that our value
+  // is larger. We can do this with select1, combined with search or scanning.
+  // We can also do this with scanning without select1. Most probably the
+  // fastest way for regular cases.
+
+  // scan potential solutions
+  int templValue = value & d.lMask;
+
+  while{
+    while(buf==0){
+      pos64 += 64;
+      buf = d.b[pos64>>6];
+    }
+
+    pos63 = buf.toRadixString(2).split('').reversed.takeWhile((e) => e == '0').length;
+
+    // check potential solution
+    int hVal = pos64 + pos63 - k;
+
+
+    if(hValue < hVal || templValue <= lValue(k, d)){
+      break;
+    }
+
+    buf &= buf - 1;
+    k++;
+  }
+  return Tuple2(k, (pos64+pos63-k)<<d.sizeLValue | lValue(k, d));
+}
